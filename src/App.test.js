a55726e06e7e4b70a4536a80a0b0997025a2e7bd
Mironov Boris <lboris201@yandex.ru>
2025-11-23 08:52:37 +0300

    Removed extra files

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders technologies heading", () => {
-    render(<App />);
-    const heading = screen.getByText(/Technologies:/i);
-    expect(heading).toBeInTheDocument();
-});
